e5887b4d1d1cff31b429f0bce0790cb5ab2e945e
Emmanuel-Bakari <emmanuelbakari18@gmail.com>
2024-09-21 14:45:03 +0300

    week 4 dart

---
+++ week 4.dart
@@ -0,0 +1,35 @@
+Concatenation: Hello, Dart!
+Interpolation: Welcome, Dart, to the Dart Program!
+Substring (first 3 chars): Dar
+Uppercase: DART
+Lowercase: dart
+Reversed: traD
+Length: 4
+
+List Example:
+Apple
+Mango
+Orange
+
+Set Example (No duplicates):
+Apple
+Mango
+Banana
+Grapes
+
+Map Example (Stock of Fruits):
+Apple: 10
+Mango: 7
+Orange: 12
+
+File Contents:
+This is a Dart file handling example.
+
+File data written successfully into 'output.txt'.
+
+Current DateTime: 2024-09-21 14:52:35.123456
+Next week: 2024-09-28 14:52:35.123456
+Last week: 2024-09-14 14:52:35.123456
+Difference between now and January 1st, 2024: 264 days
+
+Please enter a string:
